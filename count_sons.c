bc9698baba8cd6dc64ba0933420f50fabe56a018
Sergey <ser.buh2@gmail.com>
2014-11-10 02:33:56 +0100

    merge with Sergey's count_sons.c

--- count_sons.c
+++ count_sons.c
@@ -1,11 +1,12 @@
-	#include <../include/linux/errno.h>
-	#include <../include/linux/sched.h>
-	#include <../include/linux/kernel.h>
+#include <../include/linux/errno.h>
+#include <../include/linux/sched.h>
+#include <../include/linux/kernel.h>
+
 int sys_slow_count_sons(int pid)
 {
-	if(pid < 0)
+	if (pid < 0)
 		return -EINVAL;
-	if(pid == 0)
+	if (pid == 0)
 		return 1;
 	struct task_struct *pd = find_task_by_pid(pid);
 	if(pd->state == TASK_ZOMBIE)
@@ -14,7 +15,7 @@ int sys_slow_count_sons(int pid)
 		return -ESRCH;
 	int sonCounter = 0;
 	struct task_struct *child = pd->p_cptr;
-	while(child != NULL){
+	while (child != NULL){
 		sonCounter++;
 		child = child->p_osptr;
 	}
@@ -23,11 +24,13 @@ int sys_slow_count_sons(int pid)
 
 int sys_fast_count_sons(int pid)
 {
-	if(pid < 0)
+	if (pid < 0)
 		return -EINVAL;
-	if(pid == 0)
+	if (pid == 0)
 		return 1;
 	struct task_struct *pd = find_task_by_pid(pid);
+	//printk("### sys_fast_count_sons ");
+	//printk("### pid: %d ### p_opptr: %d ### p_pptr: %d ### numberOfSons: %d ###\n", pd->pid, pd->p_opptr->pid, pd->p_pptr->pid, pd->numberOfSons);
 	if(pd == NULL)
 		return -ESRCH;
 	return pd->numberOfSons;
