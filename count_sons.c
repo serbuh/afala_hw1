e37a0b570da8490fef9f2ec4cbc5287c597e7259
Sergey <ser.buh2@gmail.com>
2014-11-12 00:21:35 +0100

    restore count_sons.c

--- count_sons.c
+++ count_sons.c
@@ -1,28 +1,21 @@
-	#include <../include/linux/errno.h>
-	#include <../include/linux/sched.h>
-	#include <../include/linux/kernel.h>
-
-struct task_struct* checkPid(int pid){
-	struct task_struct* pd = NULL;
-	if(pid == 0){
-		pd = &init_task;
-		return pd->p_pptr;
-	} else	
-		return find_task_by_pid(pid);
-}
+#include <../include/linux/errno.h>
+#include <../include/linux/sched.h>
+#include <../include/linux/kernel.h>
 
 int sys_slow_count_sons(int pid)
 {
-	if(pid < 0)
+	if (pid < 0)
 		return -EINVAL;
-	struct task_struct *pd = checkPid(pid);
+	if (pid == 0)
+		return 1;
+	struct task_struct *pd = find_task_by_pid(pid);
 	if(pd->state == TASK_ZOMBIE)
 		return 0;
 	if(pd == NULL)
 		return -ESRCH;
 	int sonCounter = 0;
 	struct task_struct *child = pd->p_cptr;
-	while(child != NULL){
+	while (child != NULL){
 		sonCounter++;
 		child = child->p_osptr;
 	}
@@ -31,9 +24,13 @@ int sys_slow_count_sons(int pid)
 
 int sys_fast_count_sons(int pid)
 {
-	if(pid < 0)
+	if (pid < 0)
 		return -EINVAL;
-	struct task_struct *pd = checkPid(pid);
+	if (pid == 0)
+		return 1;
+	struct task_struct *pd = find_task_by_pid(pid);
+	//printk("### sys_fast_count_sons ");
+	//printk("### pid: %d ### p_opptr: %d ### p_pptr: %d ### numberOfSons: %d ###\n", pd->pid, pd->p_opptr->pid, pd->p_pptr->pid, pd->numberOfSons);
 	if(pd == NULL)
 		return -ESRCH;
 	return pd->numberOfSons;
