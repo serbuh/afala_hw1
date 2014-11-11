3ea063d537aa033d05fde27d14ac24271a637b02
shadows89 <shadow89@t2.technion.ac.il>
2014-11-12 00:55:59 +0200

    preaty

--- count_sons.c
+++ count_sons.c
@@ -4,10 +4,10 @@
 
 struct task_struct* checkPid(int pid){
 	struct task_struct* pd = NULL;
-	if(pid == 0){
+	if (pid == 0) {
 		pd = &init_task;
 		return pd->p_pptr;
-	} else	
+	}  else	
 		return find_task_by_pid(pid);
 }
 
@@ -33,9 +33,7 @@ int sys_fast_count_sons(int pid)
 {
 	if (pid < 0)
 		return -EINVAL;
-	struct task_struct *pd = checkPid(pid);;
-	//printk("### sys_fast_count_sons ");
-	//printk("### pid: %d ### p_opptr: %d ### p_pptr: %d ### numberOfSons: %d ###\n", pd->pid, pd->p_opptr->pid, pd->p_pptr->pid, pd->numberOfSons);
+	struct task_struct *pd = checkPid(pid);
 	if(pd == NULL)
 		return -ESRCH;
 	return pd->numberOfSons;
